54e3a3d3d56fa1024670ec92fffab4b347febf16
Evans Ngebe <evanskatana7025@gmail.com>
2024-03-26 19:32:23 +0300

    Commented error reporting

--- employees/leave-report.php
+++ employees/leave-report.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 include('../includes/dbconn.php');
 
 if (strlen($_SESSION['alogin']) == 1) {
